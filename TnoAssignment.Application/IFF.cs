3196488c35ba866995584f71d9a5083447e59e74
carvex21 <anas.valeras@gmail.com>
2025-03-15 13:25:56 +0100

    fix: throwing an exception if the scanned unit is an empty string

--- TnoAssignment.Application/IFF.cs
+++ TnoAssignment.Application/IFF.cs
@@ -4,6 +4,10 @@ public abstract class Iff
 {
     public static bool IsHostile(string unit)
     {
+        if (unit == "")
+        {
+            throw new Exception("Empty unit value detected");
+        }
         var oneCount = unit.Count(c => c == '1');
         var zeroCount = unit.Count(c => c == '0');
         return oneCount > zeroCount;
