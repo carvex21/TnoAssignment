e761c951bbcb646a35509606240fddf43d527cfc
carvex21 <anas.valeras@gmail.com>
2025-03-15 13:26:30 +0100

    tests: add unit tests for IFF class

---
+++ TnoAssignment.Application.Tests/IffTests.cs
@@ -0,0 +1,48 @@
+﻿using Shouldly;
+
+namespace TnoAssignment.Application.Tests;
+
+public class IffTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void IsHostile_ShouldReturnFalse_WhenZeroesAreMoreThanOnes()
+    {
+        // Assign
+        var unit = "0101010000101000010";
+
+        // Act
+        var isHostile = Iff.IsHostile(unit);
+
+        // Assert
+        isHostile.ShouldBeFalse();
+    }
+
+    [Test]
+    public void IsHostile_ShouldReturnTrue_WhenOnesAreMoreThanZeroes()
+    {
+        // Assign
+        var unit = "0101010111111111111010";
+
+        // Act
+        var isHostile = Iff.IsHostile(unit);
+
+        // Assert
+        isHostile.ShouldBeTrue();
+    }
+
+    [Test]
+    public void IsHostile_ShouldThrow_WhenUnitStringIsEmpty()
+    {
+        // Assign
+        var unit = "";
+
+        // Act
+        // Assert
+        Should.Throw<Exception>(() => Iff.IsHostile(unit));
+    }
+}
